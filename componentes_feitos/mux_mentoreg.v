bd143c773eb499063117c0c1aebab08e4e5de0ff
jrsm-hub <jrsm@cin.ufpe.br>
2021-11-19 14:55:58 -0300

    Update mux_mentoreg.v
    Ajustes

--- componentes_feitos/mux_mentoreg.v
+++ componentes_feitos/mux_mentoreg.v
@@ -1,5 +1,5 @@
-module mux_mentoreg (
-    input wire [3:0] mentoreg,
+module mux_MENtoReg (
+    input wire [3:0] MENtoReg,
     input wire [31:0] ALU_out,
     input wire [31:0] LS_out,
     input wire [31:0] HI_out,
@@ -8,30 +8,30 @@ module mux_mentoreg (
     input wire [31:0] result,
     input wire [31:0] Shift_Left_16_out,
     input wire [31:0] Shift_Reg_out,
-    output reg [31:0] mentoreg_out
+    output reg [31:0] MENtoReg_out
     
 );
 
 always @(*) begin
-    case(mentoreg)
+    case(MENtoReg)
         4'b0000:
-            mentoreg_out <= HI_out;
+            MENtoReg_out <= HI_out;
         4'b0001:
-            mentoreg_out <= LO_out;
+            MENtoReg_out <= LO_out;
         4'b0010:
-            mentoreg_out <= result;
+            MENtoReg_out <= result;
         4'b0011:
-            mentoreg_out <= LS_out;
+            MENtoReg_out <= LS_out;
         4'b0100:
-            mentoreg_out <= Shift_Left_16_out;
+            MENtoReg_out <= Shift_Left_16_out;
         4'b0101:
-            mentoreg_out <= ALU_out;
+            MENtoReg_out <= ALU_out;
         4'b0110:
-            mentoreg_out <= Shift_Reg_out;
+            MENtoReg_out <= Shift_Reg_out;
         4'b0111:
-            mentoreg_out <= Sign_extend_1to32_out;
+            MENtoReg_out <= Sign_extend_1to32_out;
         4'b1000:
-            mentoreg_out <= 32'd227;
+            MENtoReg_out <= 32'd227;
     endcase
 end
 
