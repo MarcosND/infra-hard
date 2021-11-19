81ad374653b5977829b153d747f9d5388f522b19
jrsm-hub <jrsm@cin.ufpe.br>
2021-11-19 15:57:30 -0300

    Create mux_Hi.v
    Ajustes

---
+++ componentes_feitos/mux_Hi.v
@@ -0,0 +1,15 @@
+module mux_Hi (
+    input wire [1:0] seletor, \\Não tenho certeza se são 2 bits
+    input wire [31:0] Hi0_out,
+    input wire [31:0] Hi1_out,
+    output wire [31:0] result
+);
+always @(*) begin 
+        case(seletor)
+            2'b00:
+                result <= Hi0_out;
+            2'b01:
+                result <= Hi1_out;                        
+        endcase
+end    
+endmodule
