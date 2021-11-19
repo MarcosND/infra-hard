ff2e37ed71687d4de78426a25f6c2929245095ee
Otávio Augusto <otavioaugustomachadodeoliveira@gmail.com>
2021-11-19 20:02:05 -0300

    atualizando unit control

--- componentes_feitos/ctrl_unit.v
+++ componentes_feitos/ctrl_unit.v
@@ -5,18 +5,22 @@ module ctrl_unit {
     
 
 
-    //fios de controle
-    output reg      PC_write,
-    output reg     MEM_write,    
-    output reg      IR_write,
-    output reg      RB_w,
-    output reg      AB_w,
-    output reg      Regwrite,
-    output reg      AluSrcA,
-    output reg      AluSrcB,
+    //fios de controle 
+    output reg          PC_write,
+    output reg          MEM_write,    
+    output reg          IR_write,
+    output reg          RB_w,
+    output reg          AB_w,
+    output reg          Regwrite,
+    output reg [1:0]    AluSrcA,
+    output reg [1:0]    AluSrcB, 
                     
-    output reg  [2:0]    Alu_control,
-    
+    output reg      [2:0] Alu_control,
+    output reg      [3:0] MEMtoReg, 
+    output reg      [2:0] PCsource ,
+    output reg      [1:0] Exception,
+    output reg      [2:0] IorD,
+
     
     
     //controladores dos muxes
@@ -25,7 +29,7 @@ module ctrl_unit {
     output reg      M_ULAB,
     
     //Controlador especial para instrução reset
-    output reg      rst_out
+    output reg      rst_out,
     
     
     //Flags
@@ -37,21 +41,37 @@ module ctrl_unit {
     input wire      Lt,
     
     //Fios de Dados
-    input wire [5:0]        OPCODE,
-    input wire [4:0]        RS,
-    input wire [4:0]        RT,
-    input wire [15:0]       OFFSET,
-    input wire [4:0]        WriteReg_in
-    input wire [31:0]       ULA_out,
-    input wire [31:0]       PC_out,
-    input wire [31:0]       MEM_out,
-    input wire [31:0]       regA_out,
-    input wire [31:0]       regB_out,
-    input wire [31:0]       B_out,
-    input wire [31:0]       A_out,
-    input wire [31:0]       SE16_out,
-    input wire [31:0]       AluA_out,
-    input wire [31:0]       AluB_out,
+   input wire [5:0] OPCODE,
+   input wire [4:0] RS,
+   input wire [4:0] RT,
+   input wire [15:0] OFFSET,
+   input wire [4:0] WriteReg_in,
+   input wire [31:0] ULA_out,
+   input wire [31:0] PC_out,
+   input wire [31:0] MEM_out,
+   input wire [31:0] regA_out,
+   input wire [31:0] regB_out,
+   input wire [31:0] B_out,
+   input wire [31:0] A_out,
+   input wire [31:0] SE16_out,
+   input wire [31:0] AluA_out,
+   input wire [31:0] AluB_out,
+   input wire [2:0] PCsource,
+   input wire [31:0] Shift_left_26to28_out,
+   input wire [31:0] EPC_out,
+   input wire [31:0] result,
+   input wire [31:0] ALU_out,
+   input wire [31:0] PCsource_out,
+   input wire [31:0] LS_out,
+   input wire [31:0] HI_out,
+   input wire [31:0] LO_out,
+   input wire [31:0] Sign_extend_1to32_out,
+   input wire [31:0] Shift_Left_16_out,
+   input wire [31:0] Shift_Reg_out,
+   input wire [31:0] MEMtoReg_out,
+   input wire [31:0] Excepction_out,
+   input wire [31:0] Expction_out,
+   input wire [31:0] IorD_out,
     
 
 
@@ -70,10 +90,13 @@ reg[2:0] COUNTER;
 // Parametros (Constantes)
 
     //Estados principais da maquina
-    parameter $T_COMMON = 2'b00 ;
-    parameter ST_ADD = 2'b01;
-    parameter ST_ADDI = 2'b10;
-    parameter ST_RESET = 2'b11 ;
+    parameter ST_FETCH_1 = 3'b000 ;
+    parameter ST_FETCH_2 = 3'b001 ;
+    parameter DECODE = 3'b010;
+    parameter ST_ADD_1 = 3'b011;
+    parameter ST_ADD_2 = 3'b100;
+    parameter ST_RESET = 3'b101 ;    
+    parameter ST_CLOSE_WRITE = 3'b110
 
     //Opcode codes as aliases
     parameter  ADD = 6'b000000 ;
@@ -87,252 +110,167 @@ initial begin
 end
 
 always @(posedge clk) begin
-    if (reset == 1'b1) begin
-    //resolve problema do reset poder ter vindo de 2 lugares: apertado o botão e entrado no reset
-      
-      if (STATE != ST_RESET)begin
-        
-        STATE = ST_RESET;
-
-        //Define os sinais
-        PC_write        =   1'b0;
-       MEM_write       =   1'b0;
-        riteIR_write        =   1'b0;
-        RB_w        =   1'b0;
-        AB_w        =   1'b0;
-        Alu_control       =   3'b000;
-        M_writeReg      =   1'b0;
-       mux_ulaA      =   1'b0;
-        M_ULAB      =   2'b00;
-        rst_out     =   1'b1; //
-
-        // define o contador para a próxima operação
-        COUNTER = 3'b000;
-      end
-        else begin
-         
-         STATE = ST_COMMON;
-         //Define os sinais
-        PC_write        =   1'b0;
-       MEM_write       =   1'b0;
-        riteIR_write        =   1'b0;
-        RB_w        =   1'b0;
-        AB_w        =   1'b0;
-        Alu_control       =   3'b000;
-        M_writeReg      =   1'b0;
-       mux_ulaA      =   1'b0;
-        M_ULAB      =   2'b00;
-        rst_out     =   1'b0; // 
-
-        // define o contador para a próxima operação
-        COUNTER = 3'b000;
+        if (reset)begin
           
-        end
+       
 
+        //Define os sinais, zerando eles
+        PC_write            = 1'b0;
+        MEM_write           = 1'b0;
+        IR_write            = 1'b0;
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b1;
+        AluSrcA             = 2'b00;
+        AluSrcB             = 2'b00;
+        [2:0] Alu_control   = 2'b0;
+        [3:0] MEMtoReg      = 4'b1000;
+        [2:0] PCsource      = 2'b0;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0;
+        
+        
+         end
+
+        STATE = ST_FETCH_1:
     
-    end
 
     
     
     else begin
         case (STATE)
-            ST_COMMON: begin
+            ST_FETCH_1: begin
+             //Define os sinais
+        PC_write            = 1'b0; 
+        MEM_write           = 1'b0;
+        IR_write            = 1'b0;
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b0; // 
+        AluSrcA             = 2'b00;
+        AluSrcB             = 2'b01; // 
+        [2:0] Alu_control   = 2'b0;
+        [3:0] MEMtoReg      = 4'b0000; //
+        [2:0] PCsource      = 2'b0;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0;   
+                
+        STATE = ST_FETCH_2;   
             
-            if (COUNTER == 3'b000 || COUNTER == 3'b001 | COUNTER == 3'b010 )begin
-                 STATE = ST_COMMON;
-                 //Define os sinais
-                PC_write        =   1'b0;
-               MEM_write       =   1'b0; // 
-                riteIR_write        =   1'b0;
-                RB_w        =   1'b0;
-                AB_w        =   1'b0;
-                Alu_control       =   3'b001; // 
-                M_writeReg      =   1'b0;
-               mux_ulaA      =   1'b0;//
-                M_ULAB      =   2'b01; //
-                rst_out     =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = COUNTER +1;
+        end
             
-                end
-            else if (COUNTER == 3'b011) begin
-                STATE = ST_COMMON;
-                 //Define os sinais
-                PC_write        =   1'b1; //
-               MEM_write       =   1'b0; 
-                riteIR_write        =   1'b1; //
-                RB_w        =   1'b0;
-                AB_w        =   1'b0;
-                Alu_control       =   3'b001; 
-                M_writeReg      =   1'b0;
-               mux_ulaA      =   1'b0;
-                M_ULAB      =   2'b01; 
-                rst_out     =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = COUNTER +1;
-
-
-            end
-            else if(COUNTER == 3'b100 ) begin
-               STATE = ST_COMMON;
-                //Define os sinais
-                PC_write        =   1'b0; //
-               MEM_write       =   1'b0; 
-                riteIR_write        =   1'b0; //
-                RB_w        =   1'b0;
-                AB_w        =   1'b1; // 
-                Alu_control       =   3'b000; 
-                M_writeReg      =   1'b0;
-               mux_ulaA      =   1'b0;
-                M_ULAB      =   2'b00; 
-                rst_out     =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = COUNTER +1;
-
-            end
+        ST_FETCH_2: begin
+           
+              //Define os sinais
+        PC_write            = 1'b1; // 
+        MEM_write           = 1'b0;
+        IR_write            = 1'b0;
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b0; 
+        AluSrcA             = 2'b00;
+        AluSrcB             = 2'b01; 
+        [2:0] Alu_control   = 2'b0;
+        [3:0] MEMtoReg      = 4'b0000; 
+        [2:0] PCsource      = 2'b10;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0; 
         
-            else if(COUNTER == 3b101) begin
-                case (OPCODE)
-                    ADD: begin
-                      STATE = ST_ADD;
-                    end
-                    ADDI: begin
-                      STATE = ST_ADDI;
-                    end
-                    RESET: begin
-                      STATE = ST_RESET;
-                    end
+        STATE = ST_DECODE;
+        
+        end
+            ST_DECODE: begin
+             //Define os sinais
+        PC_write            = 1'b1;  
+        MEM_write           = 1'b0;
+        IR_write            = 1'b1; //
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b0; 
+        [1:0]AluSrcA        = 2'b00; //
+        [1:0]AluSrcB        = 2'b10; //
+        [2:0] Alu_control   = 2'b01;
+        [3:0] MEMtoReg      = 4'b0000; 
+        [2:0] PCsource      = 2'b10;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0; 
+        
+        
+            
+        end
+            ST_ADD_1: begin
+             //Define os sinais
+        PC_write            = 1'b1;  
+        MEM_write           = 1'b0;
+        IR_write            = 1'b1; 
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b0; 
+        [1:0]AluSrcA        = 2'b01; //
+        [1:0]AluSrcB        = 2'b00; //
+        [2:0] Alu_control   = 2'b01;
+        [3:0] MEMtoReg      = 4'b0000; 
+        [2:0] PCsource      = 2'b10;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0; 
+            
+                STATE = ST_ADD_2
+        end
+            ST_ADD_2: begin
+              //Define os sinais
+        PC_write            = 1'b1;  
+        MEM_write           = 1'b0;
+        IR_write            = 1'b1; 
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b1; //
+        [1:0]AluSrcA        = 2'b01; 
+        [1:0]AluSrcB        = 2'b00; 
+        [2:0] Alu_control   = 2'b01;
+        [3:0] MEMtoReg      = 4'b0010; //
+        [2:0] PCsource      = 2'b10;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0; 
+        
 
+        STATE = CLOSE_WRITE;
 
-                endcase
+            end
+        ST_CLOSE_WRITE: begin
+               //Define os sinais, vai zerar tudo
+        PC_write            = 1'b0;  
+        MEM_write           = 1'b0;
+        IR_write            = 1'b0; 
+        RB_w                = 1'b0;
+        AB_w                = 1'b0;
+        Regwrite            = 1'b0; 
+        [1:0]AluSrcA        = 2'b01; 
+        [1:0]AluSrcB        = 2'b00; 
+        [2:0] Alu_control   = 2'b01;
+        [3:0] MEMtoReg      = 4'b0000; 
+        [2:0] PCsource      = 2'b00;
+        [1:0] Exception     = 1'b0;
+        [2:0] IorD          = 2'b0; 
+        
+        
+        
+
+        end
+
+        
+
+            
+            
+            
+            
+            
+            
+            
+            
                 
-                
-                //Define os sinais
-                PC_write        =   1'b0; 
-               MEM_write       =   1'b0; 
-                riteIR_write        =   1'b0; 
-                RB_w        =   1'b0;
-                AB_w        =   1'b0; //  
-                Alu_control       =   3'b000; 
-                M_writeReg      =   1'b0;
-               mux_ulaA      =   1'b0;
-                M_ULAB      =   2'b00; 
-                rst_out     =   1'b0; 
 
-                // define o contador para a próxima operação
-                COUNTER = 3'b000;
-              
-
-            end
 
             
-            end
-
-            ST_ADD: begin
-            if (COUNTER == 3'b000) begin
-                STATE = ST_ADD;
-                //Define os sinais
-                PC_write        =   1'b0; //
-               MEM_write       =   1'b0; 
-                riteIR_write        =   1'b0; //
-                RB_w        =   1'b1;
-                AB_w        =   1'b0; // 
-                Alu_control       =   3'b001; 
-                M_writeReg      =   1'b1;
-               mux_ulaA      =   1'b1;
-                M_ULAB      =   2'b00; 
-                rst_out     =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = COUNTER +1;
-            
-                end
-
-            else if (COUNTER == 3'b001)begin
-                STATE = ST_COMMON;
-                //Define os sinais
-                PC_write        =   1'b0; 
-               MEM_write       =   1'b0;
-                riteIR_write        =   1'b0; 
-                RB_w        =   1'b1;
-                AB_w        =   1'b0; 
-                Alu_control       =   3'b001; 
-                M_writeReg      =   1'b1;
-               mux_ulaA      =   1'b1;
-                M_ULAB      =   2'b00; 
-                rst_out     =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = 3'b000;
-
-
-            end
-            ST_ADDI: begin
-            if (COUNTER == 3'b000) begin
-                STATE = ST_ADDI;
-                //Define os sinais
-                PC_write        =   1'b0; //
-               MEM_write       =   1'b0; 
-                riteIR_write        =   1'b0; //
-                RB_w        =   1'b1;
-                AB_w        =   1'b0; // 
-                Alu_control       =   3'b001; 
-                M_writeReg      =   1'b0;
-               mux_ulaA      =   1'b1;
-                M_ULAB      =   2'b10; 
-                rst_out     =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = COUNTER +1;
-            
-                end
-
-            else if (COUNTER == 3'b001)begin
-                STATE = ST_COMMON;
-                //Define os sinais
-                PC_write        =   1'b0; 
-                MEM_write       =   1'b0;
-                riteIR_write    =   1'b0; 
-                RB_w            =   1'b1;
-                AB_w            =   1'b0; 
-                Alu_control           =   3'b001; 
-                M_writeReg          =   1'b0;
-               mux_ulaA          =   1'b1;
-                M_ULAB          =   2'b10; 
-                rst_out         =   1'b0; 
-
-                // define o contador para a próxima operação
-                COUNTER = 3'b000;
-
-
-            end
-            
-            
-            ST_RESET: begin
-             if (COUNTER == 3'b000) begin
-                STATE = ST_RESET;
-                //Define os sinais
-                PC_write        =   1'b0; //
-               MEM_write       =   1'b0; 
-                riteIR_write        =   1'b0; //
-                RB_w        =   1'b0;
-                AB_w        =   1'b0; // 
-                Alu_control       =   3'b000; 
-                M_writeReg      =   1'b0;
-               mux_ulaA      =   1'b0;
-                M_ULAB      =   2'b00; 
-                rst_out     =   1'b1; 
-
-                // define o contador para a próxima operação
-                COUNTER = 3'b000;
-            
-                end
-
-
-            end
 
         endcase     
     
