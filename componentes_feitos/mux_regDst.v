f747ddf359a8f972a3f447ba60a81dd8fd6c8635
Olavo Rangel <olavo.rangel@elogroup.com.br>
2021-11-18 22:43:46 -0300

    CPU: Ajeitar problemas da muxRegDst

--- componentes_feitos/mux_regDst.v
+++ componentes_feitos/mux_regDst.v
@@ -1,12 +1,12 @@
 module mux_regDst(
     input wire [1:0] seletor,
-    input wire [31:0] Register,
-    input wire  [31:0] instruction15_11,
+    input wire [4:0] registerRT,
+    input wire  [15:0] instruction15_11,
     output wire [31:0] mux_regDst_out
 )
 
-assign mux_regDst_out = (seletor == 2'b00) ? Register : (seletor == 2'b10) ? 32'd29: 
-                      (seletor == 2'b01) ? instruction15_11 : (seletor == 2'b11) ? 32'd31;
+assign mux_regDst_out = (seletor == 2'b00) ? RegisterRT : (seletor == 2'b10) ? 5'd29: 
+                      (seletor == 2'b01) ? instruction15_11[15:11] : (seletor == 2'b11) ? 5'd31;
 
 
 endmodule
