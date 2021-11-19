5ec0b88ab39167e4d1210a549b554811e7156fde
jrsm-hub <jrsm@cin.ufpe.br>
2021-11-19 15:31:11 -0300

    Create mux_IorD.v
    Ajustes

---
+++ componentes_feitos/mux_IorD.v
@@ -0,0 +1,21 @@
+module mux_IorD (
+    input wire [2:0] IorD
+    input wire [31:0] PC_out,
+    input wire [31:0] Expction_out,
+    input wire [31:0] ALU_out,
+    input wire [31:0] result,
+    output wire [31:0] IorD_out
+);
+always @(*) begin
+        case (IorD)
+            2'b00:
+                IorD_out <= PC_out;
+            2'b01:
+                IorD_out <= Expction_out;
+            2'b10:
+                IorD_out <= ALU_out;
+            2'b11:
+                IorD_out <= result;    
+        endcase
+end
+endmodule
