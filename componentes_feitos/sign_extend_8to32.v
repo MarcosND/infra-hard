c465d280ecb8e0471bf5167e4fd927b9c810c594
Marcos André <macam@cin.ufpe.br>
2021-11-20 00:14:44 -0300

    (main): fixed syntax errors

--- componentes_feitos/sign_extend_8to32.v
+++ componentes_feitos/sign_extend_8to32.v
@@ -2,5 +2,5 @@ module sign_extend_8to32 (
     input wire [7:0] MD_out,
     output wire [31:0] resultado
 );
-    assign resultado = {{24{1b0}}, MD_out}
+    assign resultado = {{24{1'b0}}, MD_out};
 endmodule
