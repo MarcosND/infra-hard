f716e63c6f5bb1f0951b64ed400f81ab0d02c104
Olavo Rangel <olavo.rangel@elogroup.com.br>
2021-11-23 20:31:00 -0300

    shift-left: Cansaço mental me consome

--- componentes_feitos/shift_left_16.v
+++ componentes_feitos/shift_left_16.v
@@ -2,9 +2,9 @@ module shift_left_16(
     input wire [15:0] ShiftLeft16_input,
     output wire [31:0] Shifleft16_output
 );
-    wire [31:0] Aux;
+    wire [31:0] help;
 
-    assign Aux = {{16{1'b0}}, ShiftLeft16_input};
+    assign help = {{16{1'b0}}, ShiftLeft16_input};
     assign Shifleft2_out = ShiftLeft16_input << 16; 
 
 endmodule
